30ed65bf2cb1615794bb92d202d082690d40566c
Ashley <ashley.martinez32@gmail.com>
2023-02-22 15:12:08 -0600

    completed js for min and hr hand

--- clock.js
+++ clock.js
@@ -1,15 +1,23 @@
 // targeting class hand second-hand
-const secondHand = document.querySelector('.second-hand');
+const secHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.minute-hand');
+const hrHand = document.querySelector('.hour-hand');
+
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds)
+    secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    // console.log(seconds)
     const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
     const hours = now.getHours();
-    // console.log(hours,minutes,seconds)
+    console.log("this is the hours: "+ hours);
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hrHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    console.log(now);
 }
 
 
-setInterval(setDate, 1000,secondHand)
+setInterval(setDate, 1000,secHand)
