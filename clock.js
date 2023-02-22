510150e641824e2c6d84b323a3be87555c0b419d
Ashley <ashley.martinez32@gmail.com>
2023-02-22 14:31:36 -0600

    completed js for sec hand

---
+++ clock.js
@@ -0,0 +1,15 @@
+// targeting class hand second-hand
+const secondHand = document.querySelector('.second-hand');
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(seconds)
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    // console.log(hours,minutes,seconds)
+}
+
+
+setInterval(setDate, 1000,secondHand)
